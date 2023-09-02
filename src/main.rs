cf0e629826d9d49af9024c29204a8b269acf27c7
Chris West (Faux) <git@goeswhere.com>
2023-09-02 16:58:00 +0100

    chore: extract method

--- src/main.rs
+++ src/main.rs
@@ -5,44 +5,65 @@ use base64::engine::general_purpose::STANDARD as b64;
 use base64::Engine;
 use chrono::Utc;
 use hmac::Mac;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
 use serde_json::{json, Value};
 
 type HmacSha1 = hmac::Hmac<sha1::Sha1>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let api = env_var("SOLIS_API")?;
-    let api = api.trim_end_matches('/');
-    let key = env_var("SOLIS_KEY")?;
-    let secret = env_var("SOLIS_SECRET")?;
+    let req = {
+        Req {
+            api: env_var("SOLIS_API")?.trim_end_matches('/').to_string(),
+            key: env_var("SOLIS_KEY")?,
+            secret: env_var("SOLIS_SECRET")?,
+        }
+    };
+
     let data = json!({
         "pageNo": 1,
         "pageSize": 10,
     });
+    let path = "/v1/api/userStationList";
+    let client = reqwest::Client::new();
+
+    let resp = call_api::<Value>(&client, &req, path, &data).await?;
+    println!("{}", serde_json::to_string(&resp)?);
+    Ok(())
+}
+
+struct Req {
+    api: String,
+    key: String,
+    secret: String,
+}
+
+async fn call_api<T: DeserializeOwned>(
+    client: &reqwest::Client,
+    req: &Req,
+    path: &str,
+    data: &impl Serialize,
+) -> Result<T> {
     let data = serde_json::to_vec(&data)?;
     let md5 = b64.encode(md5::compute(&data).0);
-    // TODO: +0000 instead of 'GMT'
+    // TODO: +0000 instead of 'GMT'? Doesn't seem to care
     let now = Utc::now().to_rfc2822();
-    let path = "/v1/api/userStationList";
     let param = format!("POST\n{md5}\napplication/json\n{now}\n{path}");
-    let mut mac = HmacSha1::new_from_slice(secret.as_bytes())?;
+    let mut mac = HmacSha1::new_from_slice(req.secret.as_bytes())?;
     mac.update(param.as_bytes());
     let signature = b64.encode(mac.finalize().into_bytes());
-    let client = reqwest::Client::new();
     let resp = client
-        .post(format!("{api}{path}"))
+        .post(format!("{}{path}", req.api))
         .header("Content-Type", "application/json;charset=utf-8")
         .header("Date", now)
-        .header("Authorization", format!("API {key}:{signature}"))
+        .header("Authorization", format!("API {}:{signature}", req.key))
         .header("Content-MD5", md5)
         .body(data)
         .send()
         .await?
         .error_for_status()?;
-
-    let resp = resp.json::<Value>().await?;
-    println!("{}", serde_json::to_string(&resp)?);
-    Ok(())
+    Ok(resp.json::<T>().await?)
 }
 
 fn env_var(name: &'static str) -> Result<String> {
