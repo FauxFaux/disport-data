0400c65e72aa7decd396ff9eff792c9facb73baf
Chris West (Faux) <git@goeswhere.com>
2024-02-18 16:57:48 +0000

    chore: remove (unused) sqlite

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::fs;
 
 use anyhow::Result;
 use reqwest::Client;
-use rusqlite::ToSql;
 
 use crate::config::Config;
 
@@ -35,8 +34,6 @@ async fn main() -> Result<()> {
     let config: Config = toml::from_str(&fs::read_to_string("config.toml")?)?;
 
     let http = Client::new();
-    let archive = rusqlite::Connection::open("archive.db")?;
-    migrate(&archive)?;
     let mut svcs = Vec::new();
 
     svcs.push(Service::SolisCloud(
@@ -59,44 +56,5 @@ async fn main() -> Result<()> {
         svc.run(&http).await?;
     }
 
-    Ok(())
-}
-
-fn insert(
-    conn: &rusqlite::Connection,
-    uri: impl AsRef<str>,
-    status: impl Into<u16>,
-    res: impl AsRef<str>,
-) -> Result<()> {
-    let uri = uri.as_ref();
-    let text = res.as_ref();
-    let status: u16 = status.into();
-    conn.execute(
-        "INSERT INTO archive (req, status_code, res) VALUES (?, ?, ?)",
-        [&uri as &dyn ToSql, &status, &text],
-    )?;
-    Ok(())
-}
-
-fn migrate(conn: &rusqlite::Connection) -> Result<()> {
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS migrations (
-            id INTEGER PRIMARY KEY
-        )",
-        [],
-    )?;
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS archive (
-            id INTEGER PRIMARY KEY,
-            req TEXT NOT NULL,
-            status_code INTEGER NOT NULL,
-            res TEXT NOT NULL
-        )",
-        [],
-    )?;
-    conn.execute(
-        "INSERT INTO migrations (id) VALUES (1) ON CONFLICT DO NOTHING",
-        [],
-    )?;
     Ok(())
 }
